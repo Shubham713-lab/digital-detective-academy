30da48dee87e895a4a18664523b53275b30f5aef
Ahmed Memon <ahmedmemon7x@gmail.com>
2026-07-16 14:44:55 +0500

    Updated project configuration

---
+++ lib/res/colors/app_colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFFF6BE39);
+  static const Color secondary = Color(0xFFCCC2DB);
+  static const Color tertiary = Color(0xFFA9C7FF);
+
+  static const Color background = Color(0xFF17130A);
+  static const Color surface = Color(0xFF17130A);
+
+  static const Color onPrimary = Color(0xFF402D00);
+  static const Color onSecondary = Color(0xFF332D40);
+  static const Color onTertiary = Color(0xFF003062);
+
+  static const Color text = Color(0xFFECE1D3);
+  static const Color outline = Color(0xFF9B8F7A);
+
+  static const Color error = Color(0xFFFFB4AB);
+}
